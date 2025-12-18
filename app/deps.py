b9652d907f685492674a693da96e18669e10b531
avelinofaco <facoavelino9@gmail.com>
2025-12-18 12:25:11 -0300

    autenticação incluida

--- app/deps.py
+++ app/deps.py
@@ -1,16 +1,52 @@
-# app/deps.py (síncrono)
 from typing import Generator
-from sqlalchemy.orm import Session
-from app.database import SessionLocal
 
-def get_db() -> Generator[Session, None, None]: #essa função vai gerar um Session, não recebe nada no send, e não retorna valor final.
-    """
-    Dependência FastAPI que fornece uma sessão por requisição.
-    Uso típico:
-        db: Session = Depends(get_db)
-    """
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from app.database import SessionLocal
+from app.models import Usuario
+from app.security import SECRET_KEY, ALGORITHM
+
+
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    print("token received:", token)
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int | None = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token inválido"
+            )
+    except JWTError as e:
+        print("JWT error:", e)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido ou expirado"
+        )
+
+    usuario = db.query(Usuario).filter(Usuario.id == int(user_id)).first()
+
+    if not usuario:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuário não encontrado"
+        )
+
+    return usuario
