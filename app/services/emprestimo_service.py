d4dd3511a64402c81c0615004b328fb50aac481b
avelinofaco <facoavelino9@gmail.com>
2025-12-18 19:57:04 -0300

    aplicar regras de negócio e validações

--- app/services/emprestimo_service.py
+++ app/services/emprestimo_service.py
@@ -22,8 +22,47 @@ def devolver_emprestimo_service(db: Session, emprestimo_id: int):
         raise NotFoundError(
             "Empréstimo não encontrado ou não está em estado passível de devolução."
         )
-
     return {
         "emprestimo": res["emprestimo"],
         "multa": res["multa"]
     }
+
+def renovar_emprestimo_service(db: Session, emprestimo_id: int, payload: schemas.RenovacaoEmprestimo):
+    emp = emprestimo_crud.renovar_emprestimo(db=db, emprestimo_id=emprestimo_id, dias=payload.dias)
+    if not emp:
+        raise BusinessRuleError(
+            "Empréstimo não encontrado ou não está ativo."
+        )
+    return emp
+
+
+def listar_emprestimos_service(db: Session,skip: int,limit: int):
+    items = emprestimo_crud.get_all(db, skip, limit)
+    total = emprestimo_crud.count(db)
+
+    return {
+        "total": total,
+        "skip": skip,
+        "limit": limit,
+        "items": items
+    }
+
+def listar_ativos_service(db: Session, skip: int, limit: int):
+    total = emprestimo_crud.count_ativos(db)
+    items = emprestimo_crud.get_ativos(db, skip, limit)
+
+    return {
+        "total": total,
+        "skip": skip,
+        "limit": limit,
+        "items": items
+    }
+
+ 
+def deletar_emprestimo_service(db: Session, emprestimo_id: int):
+    res = emprestimo_crud.delete(db, emprestimo_id)
+
+    if not res:
+        raise NotFoundError("Empréstimo não encontrado.")
+
+    return None
