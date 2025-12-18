45d85fb173fab97efe45457fb0e78638182ed127
avelinofaco <facoavelino9@gmail.com>
2025-12-18 12:25:49 -0300

    route login inclusa

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
-from app.routers import livros, usuarios, emprestimos, reservas, multas
+from app.routers import login, livros, usuarios, emprestimos, reservas, multas
 
 app = FastAPI(title="API Biblioteca Universitária")
 
 # Inicializa o banco de dados (cria as tabelas se não existirem)
 # init_db()
 
+app.include_router(login.router)
 app.include_router(livros.router)
 app.include_router(usuarios.router)
 app.include_router(emprestimos.router)
 app.include_router(reservas.router)
 app.include_router(multas.router)
+
 
 @app.get("/")
 def root():
