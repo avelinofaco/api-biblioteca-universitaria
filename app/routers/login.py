a140dc6a9d3e376fa40263fb4ebc35c5c86db9ce
avelinofaco <facoavelino9@gmail.com>
2025-12-18 12:30:31 -0300

    Criação da route login

---
+++ app/routers/login.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app.deps import get_db
+from app.security import verify_password, create_access_token
+from app.models import Usuario
+
+router = APIRouter(tags=["Auth"])
+
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = db.query(Usuario).filter(
+        Usuario.email == form_data.username
+    ).first()
+
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Email ou senha inválidos"
+        )
+
+    access_token = create_access_token({"sub": str(user.id)})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
