a924c76ae594d462638cc1f9e56f538761d8e991
avelinofaco <facoavelino9@gmail.com>
2025-12-18 21:20:12 -0300

    expõe endpoints REST para gerenciamento de multas

--- app/routers/multas.py
+++ app/routers/multas.py
@@ -1,49 +1,117 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from typing import List
-from sqlalchemy import select
+from fastapi import HTTPException, status
+from app.services.exceptions import NotFoundError
 
-from app.crud import multa as multa_crud
 from app import models, schemas
-from app.deps import get_db
-from app.deps import get_current_user
+from app.deps import get_db, get_current_user
+from app.services import multa_service
 from app.dependencies.permissions import exigir_roles
 
-router = APIRouter(prefix="/multas", tags=["multas"], dependencies=[Depends(exigir_roles(get_current_user))] )
+router = APIRouter(
+    prefix="/multas",
+    tags=["multas"]
+)
 
-# Listar todas as multas
-@router.get("/", response_model=List[schemas.MultaOut], description= "bibliotecario e admin podem listar multas")
-def listar_multas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), _ = Depends(exigir_roles("bibliotecario", "admin"))):
-    multas = db.execute(
-        select(models.Multa).offset(skip).limit(limit)
-    ).scalars().all()
-    return multas
-
-# Listar multas por usuário
-@router.get("/usuario/{usuario_id}", response_model=List[schemas.MultaOut])
-def multas_por_usuario(usuario_id: int, db: Session = Depends(get_db)):
-    multas = db.execute(
-        select(models.Multa)
-        .where(models.Multa.usuario_id == usuario_id)
-    ).scalars().all()
-    return multas
-
-# Pagar multa
-@router.post("/{multa_id}/pagar", response_model=schemas.MultaOut, description= "aluno pode pagar uma multa")
-def pagar_multa(multa_id: int, db: Session = Depends(get_db), _ = Depends(exigir_roles("aluno"))):
-    multa = multa_crud.pagar_multa(db, multa_id)
-    if not multa:
-        raise HTTPException(status_code=404, detail="Multa não encontrada ou já paga")
-    return multa
-
-# Buscar multa por ID
-@router.get("/{multa_id}", response_model=schemas.MultaOut)
-def get_multa(multa_id: int, db: Session = Depends(get_db)):
-    multa = multa_crud.get_by_id(db, multa_id)
-    if not multa:
-        raise HTTPException(status_code=404, detail="Multa não encontrada")
-    return multa
+# Listar todas as multas (admin / bibliotecário)
+@router.get(
+    "/",
+    response_model=schemas.PageMulta,
+    summary="Listar todas as multas",
+    description="Bibliotecário e admin podem listar todas as multas do sistema."
+)
+def listar_multas(
+    skip: int = 0,
+    limit: int = 50,
+    db: Session = Depends(get_db),
+    _ = Depends(exigir_roles("bibliotecario", "admin"))
+):
+    return multa_service.listar_multas_service(db, skip, limit)
 
 
+# Listar multas de um usuário específico (admin / bibliotecário)
+@router.get(
+    "/usuario/{usuario_id}",
+    response_model=schemas.PageMulta,
+    summary="Listar multas de um usuário",
+    description="Quais multas pertencem a esse usuário. Admin e bibliotecário podem listar multas de um usuário específico."
+)
+def multas_do_usuario(
+    usuario_id: int,
+    skip: int = 0,
+    limit: int = 10,
+    db: Session = Depends(get_db),
+    _ = Depends(exigir_roles("admin", "bibliotecario"))
+):
+    return multa_service.listar_multas_usuario_service(
+        db=db,
+        usuario_id=usuario_id,
+        skip=skip,
+        limit=limit
+    )
 
-# deve ter uma router para um usuario buscar suas multas
+
+# Listar multas do usuário logado
+@router.get(
+    "/me",
+    response_model=schemas.PageMulta,
+    summary="Listar minhas multas"
+)
+def minhas_multas(
+    skip: int = 0,
+    limit: int = 10,
+    db: Session = Depends(get_db),
+    usuario_logado: models.Usuario = Depends(get_current_user)
+):
+    return multa_service.listar_minhas_multas_service(
+        db=db,
+        usuario_id=usuario_logado.id,
+        skip=skip,
+        limit=limit
+    )
+
+
+# Buscar multa por ID (admin / bibliotecário)
+@router.get(
+    "/{multa_id}",
+    response_model=schemas.MultaOut,
+    summary="Buscar multa especifica por ID",
+    description="Quero essa multa específica, não importa de quem seja. Admin e bibliotecário podem buscar uma multa por ID. "
+)
+def recuperar_multa(
+    multa_id: int,
+    db: Session = Depends(get_db),
+    _ = Depends(exigir_roles("admin", "bibliotecario"))
+):
+    try:
+        return multa_service.recuperar_multa_service(db, multa_id)
+    except NotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+
+# Pagar multa (aluno)
+@router.post(
+    "/{multa_id}/pagar",
+    response_model=schemas.MultaOut,
+    summary="Pagar multa"
+)
+def pagar_multa(
+    multa_id: int,
+    db: Session = Depends(get_db),
+    usuario_logado: models.Usuario = Depends(get_current_user),
+    _ = Depends(exigir_roles("aluno"))
+):
+    try:
+        return multa_service.pagar_multa_service(
+            db=db,
+            multa_id=multa_id,
+            usuario_id=usuario_logado.id
+        )
+    except NotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+
